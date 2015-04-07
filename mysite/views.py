f1bdd6aacef3bf2f9650d2cd13ecc6c34c439721
mjz48 <mjz48@cornell.edu>
2015-04-06 23:26:41 -0700

    added login stuff

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 
 from mysite.models import Wallpaper
 
@@ -21,10 +25,31 @@ def resume(request):
     }
     return render(request, 'resume.html', context)
 
+
 def login_page(request):
     """ page for logging in to admin dashboard
     """
+    if request.user.is_authenticated():
+        return redirect('dashboard')
+
+    if request.method == 'POST':
+        import pdb
+        pdb.set_trace()
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')
+        else:
+            messages.add_message(request, messages.ERROR, 'Invalid credentials provided.')
+            return redirect(reverse('index'))
+
     context = {
+        'login_form': AuthenticationForm(),
         'wallpaper': Wallpaper.get_random_wallpaper(),
     }
     return render(request, 'login.html', context)
